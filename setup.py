4f60f006623e649b129c505a4eaadbcea579e07f
SebastianWellig <sebastian.wellig@gmail.com>
2020-07-09 17:33:35 +0200

    Code refactoring

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from distutils.core import setup, Extension
 
-setup(
-      ext_modules=[Extension('_vdwo', ['vdwo-python.i','src/vdwo.cpp'],
+setup(ext_modules=[Extension('_vdwo', ['vdwo-python.i','src/vdwo.cpp'],
                              include_dirs=['/usr/include/openbabel-2.0','src'],
                              libraries = ['openbabel', 'gomp'],
                              extra_compile_args=['-std=c++17', '-fopenmp'],
